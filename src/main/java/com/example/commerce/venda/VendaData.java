83fb6570dbdb7856f1f2b8992c320915e2cdf2e0
Gustavo <gustavo.zaffani@viasoft.com.br>
2018-11-30 16:02:30 -0300

    Ajuste 30/11

--- src/main/java/com/example/commerce/venda/VendaData.java
+++ src/main/java/com/example/commerce/venda/VendaData.java
@@ -1,10 +1,12 @@
 package com.example.commerce.venda;
 
+import com.example.commerce.itemVenda.ItemVenda;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface VendaData extends JpaRepository<Venda, Long> {
+
 
 }
